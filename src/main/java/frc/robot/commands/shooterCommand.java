25b37c560217204c945fc5fbc33ec774b418565a
Seankariya <sean.kariya@gmail.com>
2026-02-04 22:00:50 -0500

    coded a button to set the setpoint

--- src/main/java/frc/robot/commands/shooterCommand.java
+++ src/main/java/frc/robot/commands/shooterCommand.java
@@ -5,6 +5,7 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants.shooterConstants;
 import frc.robot.subsystems.shooter;
 
 /* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
@@ -22,7 +23,8 @@ public class shooterCommand extends Command {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    
+    //setVelocity = shooterConstants.setVelocity;
+    shootersubsystem.setShooterPID(setVelocity);
   }
 
   @Override
