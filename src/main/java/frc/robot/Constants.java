d92e9a20a48dbe7751f6722c2068b32021b97ccd
Clubstep94 <fjfcostello21@gmail.com>
2026-02-14 10:32:36 -0500

    Fixed constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -29,6 +29,7 @@ import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.RobotBase;
+import frc.lib.camera.Camera;
 import frc.robot.generated.TunerConstants;
 
 /**
@@ -188,7 +189,7 @@ public final class Constants {
         public static AprilTagFieldLayout aprilTagLayout = AprilTagFieldLayout.loadField(AprilTagFields.kDefaultField);
 
         // Camera names, must match names configured on coprocessor
-<<<<<<< HEAD
+
         public static String camera0Name = "limelight-four";
         public static String camera1Name = "limelight-three";
 
@@ -196,10 +197,9 @@ public final class Constants {
         // (Not used by Limelight, configure in web UI instead)
         public static Transform3d robotToCamera0 = new Transform3d(0.0, 0.0, 0.0, new Rotation3d(0.0, 0.0, 0.0));
         public static Transform3d robotToCamera1 = new Transform3d(0.0, 0.0, 0.0, new Rotation3d(0.0, 0.0, 0.0));
-=======
+
         public static Camera frontCamera = new Camera("FrontCam", "limelight-four", new Transform3d(0.2, 0.0, 0.2, new Rotation3d(0.0, -0.4, 0.0)));
         public static Camera rearCamera = new Camera("RearCam", "limelight-three", new Transform3d(-0.2, 0.0, 0.2, new Rotation3d(0.0, -0.4, Math.PI)));
->>>>>>> 9ad31ad6ad87598f9f10158c6ab4a17b28c01e77
 
         // Basic filtering thresholds
         public static double maxAmbiguity = 0.3;
