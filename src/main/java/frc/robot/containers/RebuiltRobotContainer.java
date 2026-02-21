09efa7dd1d554e501dc5165969551bf7dd05da23
Clubstep94 <fjfcostello21@gmail.com>
2026-02-21 14:12:47 -0500

    Added RunEverything to usable commands in Auto

--- src/main/java/frc/robot/containers/RebuiltRobotContainer.java
+++ src/main/java/frc/robot/containers/RebuiltRobotContainer.java
@@ -1,24 +1,22 @@
 package frc.robot.containers;
 
+import org.littletonrobotics.junction.networktables.LoggedDashboardChooser;
+
 import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.auto.NamedCommands;
 
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
-import frc.robot.commands.shooterCommand;
-import frc.robot.commands.Hopper.Spin;
 import frc.robot.Constants.VisionConstants;
 import frc.robot.Robot;
 import frc.robot.commands.TuningCommands;
+import frc.robot.commands.runEverything;
 import frc.robot.generated.TunerConstants;
-import frc.robot.subsystems.vision.VisionSubsystem;
 import frc.robot.subsystems.climb.ClimbModuleIOTalonFX;
 import frc.robot.subsystems.climb.ClimbSubsystem;
-import frc.robot.subsystems.feeder.FeederModuleIO;
 import frc.robot.subsystems.feeder.FeederModuleIOSparkMax;
 import frc.robot.subsystems.feeder.FeederSubsystem;
-import frc.robot.subsystems.hopper.HopperModuleIO;
 import frc.robot.subsystems.hopper.HopperModuleIOSparkMax;
 import frc.robot.subsystems.hopper.HopperSubsystem;
 import frc.robot.subsystems.intake.IntakeModuleIOSparkMax;
@@ -26,109 +24,106 @@ import frc.robot.subsystems.intake.IntakeSubsystem;
 import frc.robot.subsystems.shooter.ShooterModuleIOSparkFlex;
 import frc.robot.subsystems.shooter.ShooterSubsystem;
 import frc.robot.subsystems.swervedrive.GyroIOPigeon2;
+import frc.robot.subsystems.swervedrive.SwerveDriveSubsystem;
 import frc.robot.subsystems.swervedrive.SwerveModuleIOTalonFX;
 import frc.robot.subsystems.turret.TurretModuleIOSparkMax;
 import frc.robot.subsystems.turret.TurretSubsystem;
-import frc.robot.subsystems.swervedrive.SwerveDriveSubsystem;
 import frc.robot.subsystems.vision.VisionIOLimelight;
-
-import org.littletonrobotics.junction.networktables.LoggedDashboardChooser;
+import frc.robot.subsystems.vision.VisionSubsystem;
 
 /**
  * 
  */
 public class RebuiltRobotContainer extends RobotContainer {
 
-        /**
-         * The container for the robot. Contains subsystems, OI devices, and commands.
-         */
-        public RebuiltRobotContainer() {
-                super();
+    /**
+     * The container for the robot. Contains subsystems, OI devices, and commands.
+     */
+    public RebuiltRobotContainer() {
+        super();
 
-                // Real robot, instantiate hardware IO implementations
-                this.swerveDriveSubsystem = new SwerveDriveSubsystem(
-                                new GyroIOPigeon2(),
-                                new SwerveModuleIOTalonFX(TunerConstants.FrontLeft),
-                                new SwerveModuleIOTalonFX(TunerConstants.FrontRight),
-                                new SwerveModuleIOTalonFX(TunerConstants.BackLeft),
-                                new SwerveModuleIOTalonFX(TunerConstants.BackRight),
-                                (robotPose) -> {
-                                });
+        // Real robot, instantiate hardware IO implementations
+        this.swerveDriveSubsystem = new SwerveDriveSubsystem(
+                new GyroIOPigeon2(),
+                new SwerveModuleIOTalonFX(TunerConstants.FrontLeft),
+                new SwerveModuleIOTalonFX(TunerConstants.FrontRight),
+                new SwerveModuleIOTalonFX(TunerConstants.BackLeft),
+                new SwerveModuleIOTalonFX(TunerConstants.BackRight),
+                (robotPose) -> {
+                });
 
-                this.visionSubsystem = new VisionSubsystem(
-                                swerveDriveSubsystem::addVisionMeasurement,
-                                new VisionIOLimelight(VisionConstants.frontCamera, swerveDriveSubsystem::getRotation),
-                                new VisionIOLimelight(VisionConstants.rearCamera, swerveDriveSubsystem::getRotation));
+        this.visionSubsystem = new VisionSubsystem(
+                swerveDriveSubsystem::addVisionMeasurement,
+                new VisionIOLimelight(VisionConstants.frontCamera, swerveDriveSubsystem::getRotation),
+                new VisionIOLimelight(VisionConstants.rearCamera, swerveDriveSubsystem::getRotation));
 
-                this.m_climb = new ClimbSubsystem(new ClimbModuleIOTalonFX());
-                this.m_feeder = new FeederSubsystem(new FeederModuleIOSparkMax());
-                this.m_hopper = new HopperSubsystem(new HopperModuleIOSparkMax());
-                this.m_intake = new IntakeSubsystem(new IntakeModuleIOSparkMax());
-                this.m_shooter = new ShooterSubsystem(new ShooterModuleIOSparkFlex());
-                this.m_turret = new TurretSubsystem(new TurretModuleIOSparkMax());
+        this.m_climb = new ClimbSubsystem(new ClimbModuleIOTalonFX());
+        this.m_feeder = new FeederSubsystem(new FeederModuleIOSparkMax());
+        this.m_hopper = new HopperSubsystem(new HopperModuleIOSparkMax());
+        this.m_intake = new IntakeSubsystem(new IntakeModuleIOSparkMax());
+        this.m_shooter = new ShooterSubsystem(new ShooterModuleIOSparkFlex());
+        this.m_turret = new TurretSubsystem(new TurretModuleIOSparkMax());
 
-                //Creates the commands for using non-drive subsystems in autonomous
+        // Creates the commands for using non-drive subsystems in autonomous
 
-                NamedCommands.registerCommand("Shooter", m_shooter.shooterCommand());
-                NamedCommands.registerCommand("Feeder", m_feeder.feederCommand());
-                NamedCommands.registerCommand("Intake", m_intake.IntakeSpin());
-                NamedCommands.registerCommand("ExtendIntake", m_intake.ExtendSpin());
-                NamedCommands.registerCommand("RetractIntake", m_intake.RetractSpin());
-                NamedCommands.registerCommand("Outtake", m_intake.OuttakeSpin());
-                NamedCommands.registerCommand("Climb", m_climb.climb());
-                NamedCommands.registerCommand("Declimb", m_climb.declimb());
-                NamedCommands.registerCommand("Dontdeclimb", m_climb.dontdeclimb());
-                NamedCommands.registerCommand("SpinHopper", m_hopper.SpinCommand());
+        NamedCommands.registerCommand("Shooter", m_shooter.shooterCommand());
+        NamedCommands.registerCommand("Feeder", m_feeder.feederCommand());
+        NamedCommands.registerCommand("Intake", m_intake.IntakeSpin());
+        NamedCommands.registerCommand("ExtendIntake", m_intake.ExtendSpin());
+        NamedCommands.registerCommand("RetractIntake", m_intake.RetractSpin());
+        NamedCommands.registerCommand("Outtake", m_intake.OuttakeSpin());
+        NamedCommands.registerCommand("Climb", m_climb.climb());
+        NamedCommands.registerCommand("Declimb", m_climb.declimb());
+        NamedCommands.registerCommand("Dontdeclimb", m_climb.dontdeclimb());
+        NamedCommands.registerCommand("SpinHopper", m_hopper.SpinCommand());
+        NamedCommands.registerCommand("RunEverything", new runEverything(m_feeder, m_shooter, m_hopper));
 
+        configureAutoChooser();
+        configureButtonBindings();
 
-                configureAutoChooser();
-                configureButtonBindings();
+    }
 
-                
+    // Creates the method configureAutoChooser, which sets up the autonomous command
+    // chooser with various autonomous routines and system identification routines
+    // for the robot.
+    @Override
+    protected void configureAutoChooser() {
+        this.autoChooser = new LoggedDashboardChooser<>("Auto Choices", AutoBuilder.buildAutoChooser());
 
-        }
+        // Set up SysId routines
+        this.autoChooser.addOption(
+                "Drive Wheel Radius Characterization",
+                TuningCommands.wheelRadiusCharacterization(this.swerveDriveSubsystem));
+        this.autoChooser.addOption(
+                "Drive Simple FF Characterization",
+                TuningCommands.feedforwardCharacterization(this.swerveDriveSubsystem));
+        this.autoChooser.addOption(
+                "Drive SysId (Quasistatic Forward)",
+                this.swerveDriveSubsystem.sysIdQuasistatic(SysIdRoutine.Direction.kForward));
+        this.autoChooser.addOption(
+                "Drive SysId (Quasistatic Reverse)",
+                this.swerveDriveSubsystem.sysIdQuasistatic(SysIdRoutine.Direction.kReverse));
+        this.autoChooser.addOption(
+                "Drive SysId (Dynamic Forward)",
+                this.swerveDriveSubsystem.sysIdDynamic(SysIdRoutine.Direction.kForward));
+        this.autoChooser.addOption(
+                "Drive SysId (Dynamic Reverse)",
+                this.swerveDriveSubsystem.sysIdDynamic(SysIdRoutine.Direction.kReverse));
+    }
 
-        // Creates the method configureAutoChooser, which sets up the autonomous command
-        // chooser with various autonomous routines and system identification routines
-        // for the robot.
-        @Override
-        protected void configureAutoChooser() {
-                this.autoChooser = new LoggedDashboardChooser<>("Auto Choices", AutoBuilder.buildAutoChooser());
+    /**
+     * Use this to pass the autonomous command to the main {@link Robot} class.
+     *
+     * @return the command to run in autonomous
+     */
+    public Command getAutonomousCommand() {
+        return this.autoChooser.get();
+    }
 
-                // Set up SysId routines
-                this.autoChooser.addOption(
-                                "Drive Wheel Radius Characterization",
-                                TuningCommands.wheelRadiusCharacterization(this.swerveDriveSubsystem));
-                this.autoChooser.addOption(
-                                "Drive Simple FF Characterization",
-                                TuningCommands.feedforwardCharacterization(this.swerveDriveSubsystem));
-                this.autoChooser.addOption(
-                                "Drive SysId (Quasistatic Forward)",
-                                this.swerveDriveSubsystem.sysIdQuasistatic(SysIdRoutine.Direction.kForward));
-                this.autoChooser.addOption(
-                                "Drive SysId (Quasistatic Reverse)",
-                                this.swerveDriveSubsystem.sysIdQuasistatic(SysIdRoutine.Direction.kReverse));
-                this.autoChooser.addOption(
-                                "Drive SysId (Dynamic Forward)",
-                                this.swerveDriveSubsystem.sysIdDynamic(SysIdRoutine.Direction.kForward));
-                this.autoChooser.addOption(
-                                "Drive SysId (Dynamic Reverse)",
-                                this.swerveDriveSubsystem.sysIdDynamic(SysIdRoutine.Direction.kReverse));
-        }
+    // Called periodically in simulation
+    public void updateSimulation() {
+    }
 
-        /**
-         * Use this to pass the autonomous command to the main {@link Robot} class.
-         *
-         * @return the command to run in autonomous
-         */
-        public Command getAutonomousCommand() {
-                return this.autoChooser.get();
-        }
-
-        // Called periodically in simulation
-        public void updateSimulation() {
-        }
-
-        public void onAllianceChanged(Alliance currentAlliance, int currentLocation) {
-        }
+    public void onAllianceChanged(Alliance currentAlliance, int currentLocation) {
+    }
 }
