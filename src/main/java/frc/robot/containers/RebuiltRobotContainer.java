536782c6f7874d1960993a0c6f3c2c77912fae10
Clubstep94 <fjfcostello21@gmail.com>
2026-02-20 19:38:55 -0500

    Created an Auto using The Hopper, Feeder and Shooter

--- src/main/java/frc/robot/containers/RebuiltRobotContainer.java
+++ src/main/java/frc/robot/containers/RebuiltRobotContainer.java
@@ -1,9 +1,13 @@
 package frc.robot.containers;
 
 import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.auto.NamedCommands;
+
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
+import frc.robot.commands.shooterCommand;
+import frc.robot.commands.Hopper.Spin;
 import frc.robot.Constants.VisionConstants;
 import frc.robot.Robot;
 import frc.robot.commands.TuningCommands;
@@ -13,6 +17,7 @@ import frc.robot.subsystems.swervedrive.GyroIOPigeon2;
 import frc.robot.subsystems.swervedrive.SwerveModuleIOTalonFX;
 import frc.robot.subsystems.swervedrive.SwerveDriveSubsystem;
 import frc.robot.subsystems.vision.VisionIOLimelight;
+import frc.robot.subsystems.shooter;
 import org.littletonrobotics.junction.networktables.LoggedDashboardChooser;
 
 /**
@@ -37,10 +42,28 @@ public class RebuiltRobotContainer extends RobotContainer {
                                 });
                 this.visionSubsystem = new VisionSubsystem(
                                 swerveDriveSubsystem::addVisionMeasurement,
-                                new VisionIOLimelight(VisionConstants.frontCamera, this.swerveDriveSubsystem::getRotation),
-                                new VisionIOLimelight(VisionConstants.rearCamera, this.swerveDriveSubsystem::getRotation));
+                                new VisionIOLimelight(VisionConstants.camera0Name, swerveDriveSubsystem::getRotation),
+                                new VisionIOLimelight(VisionConstants.camera1Name, swerveDriveSubsystem::getRotation));
+
+                //Creates the commands for using non-drive subsystems in autonomous
+
+                NamedCommands.registerCommand("Shooter", m_shooter.shooterCommand());
+                NamedCommands.registerCommand("Feeder", m_feeder.feederCommand());
+                NamedCommands.registerCommand("Intake", m_intake.IntakeSpin());
+                NamedCommands.registerCommand("ExtendIntake", m_intake.ExtendSpin());
+                NamedCommands.registerCommand("RetractIntake", m_intake.RetractSpin());
+                NamedCommands.registerCommand("Outtake", m_intake.OuttakeSpin());
+                NamedCommands.registerCommand("Climb", m_climb.climb());
+                NamedCommands.registerCommand("Declimb", m_climb.declimb());
+                NamedCommands.registerCommand("Dontdeclimb", m_climb.dontdeclimb());
+                NamedCommands.registerCommand("SpinHopper", m_hopper.SpinCommand());
+
+
                 configureAutoChooser();
                 configureButtonBindings();
+
+                
+
         }
 
         // Creates the method configureAutoChooser, which sets up the autonomous command
