d113d081a618695c6ce101e5d57eafd3957d994f
krab_32 <339724932@gotvdsb.ca>
2026-02-25 11:43:28 -0500

    Changed direction of outtake

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -76,7 +76,7 @@ public final class Constants {
 
     public static class intakeConstants {
         public static final double INTAKE_SPEED = 0.5;
-        public static final double OUTTAKE_SPEED = 0.4;
+        public static final double OUTTAKE_SPEED = -0.4;
         public static final double EXTENDING_SPEED = -0.1;
         public static final double RETRACTING_SPEED = 0.2;
     }
