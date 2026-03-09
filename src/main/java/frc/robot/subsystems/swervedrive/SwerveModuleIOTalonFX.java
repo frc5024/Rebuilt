2fcfbea409b8afa1e485bbf3c44390ee2d8c8c33
jbakela <jbakela@uwo.ca>
2026-03-09 15:47:55 -0400

    Changed Settings for Auto

--- src/main/java/frc/robot/subsystems/swervedrive/SwerveModuleIOTalonFX.java
+++ src/main/java/frc/robot/subsystems/swervedrive/SwerveModuleIOTalonFX.java
@@ -1,6 +1,8 @@
 package frc.robot.subsystems.swervedrive;
 
-import static frc.robot.util.PhoenixUtil.*;
+import static frc.robot.util.PhoenixUtil.tryUntilOk;
+
+import java.util.Queue;
 
 import com.ctre.phoenix6.BaseStatusSignal;
 import com.ctre.phoenix6.CANBus;
@@ -21,6 +23,7 @@ import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 import com.ctre.phoenix6.signals.SensorDirectionValue;
 import com.ctre.phoenix6.swerve.SwerveModuleConstants;
+
 import edu.wpi.first.math.filter.Debouncer;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.util.Units;
@@ -31,8 +34,6 @@ import edu.wpi.first.units.measure.Voltage;
 import frc.robot.Constants.SwerveDriveConstants;
 import frc.robot.generated.TunerConstants;
 import frc.robot.util.PhoenixOdometryThread;
-
-import java.util.Queue;
 
 /**
  * Module IO implementation for Talon FX drive motor controller, Talon FX turn
@@ -120,7 +121,7 @@ public class SwerveModuleIOTalonFX implements SwerveModuleIO {
                     "You are using an unsupported swerve configuration, which this template does not support without manual customization. The 2025 release of Phoenix supports some swerve configurations which were not available during 2025 beta testing, preventing any development and support from the AdvantageKit developers.");
         };
         turnConfig.Feedback.RotorToSensorRatio = constants.SteerMotorGearRatio;
-        turnConfig.MotionMagic.MotionMagicCruiseVelocity = 100.0 / constants.SteerMotorGearRatio;
+        turnConfig.MotionMagic.MotionMagicCruiseVelocity = 500.0 / constants.SteerMotorGearRatio;
         turnConfig.MotionMagic.MotionMagicAcceleration = turnConfig.MotionMagic.MotionMagicCruiseVelocity / 0.100;
         turnConfig.MotionMagic.MotionMagicExpo_kV = 0.12 * constants.SteerMotorGearRatio;
         turnConfig.MotionMagic.MotionMagicExpo_kA = 0.1;
