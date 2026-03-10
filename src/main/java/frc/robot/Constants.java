2b85ade0fa4e772da19cce2a0e13d529069e2e1d
jbakela <jbakela@uwo.ca>
2026-03-10 19:04:06 -0400

    Lowered Intake Speed

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -164,7 +164,7 @@ public final class Constants {
      * 
      */
     public static class intakeConstants {
-        public static final double INTAKE_SPEED = 0.7;
+        public static final double INTAKE_SPEED = 0.6;
         public static final double OUTTAKE_SPEED = -0.4;
         public static final double EXTENDING_SPEED = -0.15;
         public static final double RETRACTING_SPEED = 0.35;
