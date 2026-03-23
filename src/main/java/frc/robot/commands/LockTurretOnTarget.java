119441bd0ee453f9d4f315b180e4df8795b3806c
John Lownie <jlownie@gmail.com>
2026-03-23 19:44:08 -0400

    updates

--- src/main/java/frc/robot/commands/LockTurretOnTarget.java
+++ src/main/java/frc/robot/commands/LockTurretOnTarget.java
@@ -5,11 +5,10 @@ import java.util.function.Supplier;
 import org.littletonrobotics.junction.Logger;
 
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Transform2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.Constants.TurretConstants;
 import frc.robot.subsystems.turret.TurretSubsystem;
 import frc.robot.util.GameUtil;
 
@@ -40,17 +39,29 @@ public class LockTurretOnTarget extends Command {
         Pose2d robotPose = robotPoseSupplier.get();
         Pose2d targetPose = GameUtil.getTargetPose(robotPose);
 
-        Translation2d turretOffset = new Translation2d(TurretConstants.OFFSET_X, TurretConstants.OFFSET_Y);
-        Translation2d turretFieldPos = robotPose.transformBy(new Transform2d(turretOffset, new Rotation2d()))
-                .getTranslation();
+        // Turret's offset from the robot's center
+        Translation2d turretOffset = new Translation2d(-0.254, 0.1778);
 
+        // Calculate the turret's position on the field
+        Translation2d turretFieldPos = robotPose.getTranslation().plus(turretOffset.rotateBy(robotPose.getRotation()));
         double distanceToTarget = turretFieldPos.getDistance(targetPose.getTranslation());
 
-        Rotation2d fieldAngle = targetPose.getTranslation().minus(turretFieldPos).getAngle();
-        Rotation2d turretAngle = fieldAngle.minus(robotPose.getRotation());
+        // Calculate the vector from the turret to the target
+        Translation2d turretToTarget = targetPose.getTranslation().minus(turretFieldPos);
+
+        // The turret's desired pose
+        Pose2d turretPose = new Pose2d(turretFieldPos, turretToTarget.getAngle());
+
+        // Calculate the desired turret angle
+        // Robot is CCW+ / Turret is CW+
+        Rotation2d turretAngle = turretPose.getRotation().minus(robotPose.getRotation()).unaryMinus();
 
         turretSubsystem.setAngle(turretAngle.getDegrees());
 
+        Logger.recordOutput("Turret/TargetAngleRad", turretAngle);
+        Logger.recordOutput("Turret/TargetAngleDeg", turretAngle.getDegrees());
+        Logger.recordOutput("Turret/TargetPose", new Pose3d(targetPose));
+        Logger.recordOutput("Turret/FieldPose", new Pose3d(turretPose));
         Logger.recordOutput("Turret/LockTurretOnTarget/DistanceToTarget", distanceToTarget);
         Logger.recordOutput("Turret/LockTurretOnTarget/RobotPoseDeg", robotPose.getRotation().getDegrees());
         Logger.recordOutput("Turret/LockTurretOnTarget/TargetPose", targetPose);
